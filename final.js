fe0d62c46b956e4eb7e47ae7c099adffb830aee8
Burhanuddin Khatri <144617735+BurhanCantCode@users.noreply.github.com>
2023-12-05 07:40:36 +0000

    added the required field alert in final.js

--- final.js
+++ final.js
@@ -1,3 +1,12 @@
 function placeOrder() {
-    alert("Your order has been placed!");
+    var cardNumber = document.getElementById("cardNumber").value;
+    var expiryDate = document.getElementById("expiryDate").value;
+    var cvv = document.getElementById("cvv").value;
+    var nameOnCard = document.getElementById("nameOnCard").value;
+    var address = document.getElementById("address").value;
+    if(!cardNumber||!expiryDate||!cvv||!nameOnCard||!address){
+        alert("Please fill the required information");
+        return;
+    }
+    alert("Payment Successful!");
 }
